ec3b062d74e1f02c2c08ea2fd2094922b1c4f522
Sérgio Júnior <sergioamjr91@gmail.com>
2023-12-03 20:31:46 -0300

    Creating main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,113 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import Link from "next/link";
+import { Open_Sans, Gochi_Hand } from "next/font/google";
+import { useState } from "react";
+import cx from "classnames";
 
-const inter = Inter({ subsets: ['latin'] })
+const OpenSans = Open_Sans({ subsets: ["latin"] });
+const gochiHand = Gochi_Hand({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
+  const [selectedNumbers, setSelectedNumbers] = useState([]);
+
+  const onAddNumber = (number) => {
+    setSelectedNumbers([...selectedNumbers, number]);
+  };
+
+  const onRemoveNumber = (number) => {
+    setSelectedNumbers(selectedNumbers.filter((n) => n !== number));
+  };
+
+  const whatsappMessage = `Olá! Estou participando da sua rifa do Luís Carlos e escolhi os números ${selectedNumbers.join(
+    ", "
+  )}. Segue o comprovante de pagamento.`;
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-full">
+      <header className={gochiHand.className}>
+        <div className="max-w-4xl m-auto px-4 py-5 flex justify-between items-center">
+          <h1 className="text-white text-5xl">Rifa do Luis</h1>
+          <nav className="flex">
+            <a className="text-white ml-3" href="#">
+              Home
+            </a>
+            <a className="text-white ml-3" href="#">
+              Prestação de contas
+            </a>
+          </nav>
         </div>
-      </div>
+      </header>
+      <section className={OpenSans.className}>
+        <div className=" max-w-4xl m-auto px-4">
+          <div className="relative">
+            <div className="grid gap-4 justify-between grid-cols-3 sm:grid-cols-5 md:grid-cols-6">
+              {Array(301)
+                .fill(1)
+                .map((_, i) => {
+                  const isSelected = selectedNumbers.includes(i);
+                  const callback = isSelected ? onRemoveNumber : onAddNumber;
+                  const btnClass = cx({
+                    "bg-white hover:bg-red-400 hover:text-white text-blue-700":
+                      isSelected,
+                    "text-white": !isSelected,
+                  });
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                  if (i == 0) return null;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                  return (
+                    <button
+                      onClick={() => callback(i)}
+                      disabled={i == 3}
+                      key={i}
+                      className={`bg-blue-500 font-semibold py-2 px-4 hover:border-transparent rounded hover:bg-blue-700 disabled:opacity-20 disabled:pointer-events-none disabled:cursor-not-allowed ${btnClass}`}
+                    >
+                      {i}
+                    </button>
+                  );
+                })}
+            </div>
+            {!!selectedNumbers.length && (
+              <div className="sticky max-w-md rounded-md bg-white left-0 right-0 bottom-6 m-auto p-3 shadow-sm border">
+                <div className="flex justify-between">
+                  <div className="w-full max-w-[calc(100%_-_110px)]">
+                    <p className="text-base text-gray-600">
+                      Números escolhidos:
+                    </p>
+                    <p className="mb-2 font-bold text-lg overflow-hidden text-ellipsis whitespace-nowrap">
+                      {[...selectedNumbers].reverse().join(", ")}
+                    </p>
+                    <p className="text-base text-gray-600">Total</p>
+                    <p className="font-bold text-lg mb-2">
+                      R${selectedNumbers.length * 10}
+                    </p>
+                    <Link
+                      className="text-blue-500 hover:text-blue-700"
+                      target="_blank"
+                      href={`https://api.whatsapp.com/send/?phone=5511973836084&text=${whatsappMessage}&type=phone_number&app_absent=0`}
+                    >
+                      🔗 Enviar comprovante por whatsapp 🔗
+                    </Link>
+                  </div>
+                  <div className="w-[100px]">
+                    <p className="font-bold text-lg text-center">PIX</p>
+                    <Image
+                      alt="QR Code PIX"
+                      className="w-full"
+                      src="/assets/qrcode-luis-carlos.png"
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </div>
+  );
 }
